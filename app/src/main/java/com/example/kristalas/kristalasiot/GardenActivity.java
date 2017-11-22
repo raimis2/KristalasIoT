9bd498f427f03443a5bccca8582b9c4e647bf72a
kristalas2@gmail.com <syncmaster920>
2017-11-22 10:38:10 +0200

    Garden UI Activity created

--- app/src/main/java/com/example/kristalas/kristalasiot/GardenActivity.java
+++ app/src/main/java/com/example/kristalas/kristalasiot/GardenActivity.java
@@ -9,5 +9,6 @@ public class GardenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_garden);
+        
     }
 }
